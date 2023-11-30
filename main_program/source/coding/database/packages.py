1cbe3aa0916d4a16a046628b27ec11b07be53252
KhodeNima <khodenimacollab@gmail.com>
2023-11-30 16:22:26 +0330

    NyvoNetHunter Project | V : 5.12.8
    API:
    
        1. Successfully implemented all of the project fundamental API request functions.

--- main_program/source/coding/database/packages.py
+++ main_program/source/coding/database/packages.py
@@ -4,19 +4,21 @@
 """
 
 from PyQt5.QtNetwork import QNetworkRequest, QNetworkAccessManager, QNetworkReply
+from abc import ABC, abstractmethod, abstractproperty
 from PyQt5.QtWidgets import QApplication, QDialog
+from requests.exceptions import ConnectionError
 from tldextract import extract as extract_url
-from PyQt5.QtCore import QByteArray
 from PyQt5.QtCore import QEventLoop
+from PyQt5.QtCore import QByteArray
 from os import system as cmd_input
 from ipaddress import ip_address
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtTest import QTest
 from PyQt5 import QtWidgets
+from typing import Literal
 import requests
 import json
 import sys
-
 
 module_is_runned_directly = __name__ == "__main__"
 
@@ -24,3 +26,5 @@ if module_is_runned_directly:
     raise PermissionError(
         "Database modules are not intended to run directly. They are produced for import usage only."
     )
+    
+
