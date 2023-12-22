85030a5051e6d5618fdf30e5151b3579c40808cb
Nima Bavar <khodenimacollab@gmail.com>
2023-12-22 11:42:22 +0330

    Main packages.py

--- main_program/source/coding/database/packages.py
+++ main_program/source/coding/database/packages.py
@@ -4,45 +4,37 @@
 """
 
 
+Try:
+    from PyQt5.QtNetwork import QNetworkRequest, QNetworkAccessManager, QNetworkReply
+    from exceptions.direct_run_error import DirectRunError
+    from abc import ABC, abstractmethod, abstractproperty
+    from PyQt5.QtWidgets import QApplication, QDialog
+    from requests.exceptions import ConnectionError
+    from tldextract import extract as extract_url
+    from PyQt5.QtCore import QPropertyAnimation
+    from requests.exceptions import SSLError
+    from PyQt5.QtCore import QEventLoop
+    from PyQt5.QtCore import QByteArray
+    from PyQt5.QtCore import pyqtSignal
+    from os import system as cmd_input
+    from dataclasses import dataclass
+    from ipaddress import ip_address
+    from PyQt5.QtCore import QThread
+    from PyQt5.QtCore import QObject
+    from typing import Literal, Dict
+    from PyQt5 import QtCore, QtGui
+    from PyQt5.QtTest import QTest
+    from platform import platform
+    from PyQt5 import QtWidgets
+    from json import loads
+    import requests
+    import socket
+    import sys
+    import os
 
-from post_launch_setup.launch_count_manager import LaunchCountManager
-launch_manager = LaunchCountManager()
 
-if launch_manager.get_launch_count() == 0:
-    from post_launch_setup.requirement_installer import install_all_requirements
-    install_all_requirements()
-    
-
-launch_manager.increase_launch_count()
-launch_manager.store_changes_to_database()
-
-
-from PyQt5.QtNetwork import QNetworkRequest, QNetworkAccessManager, QNetworkReply
-from exceptions.direct_run_error import DirectRunError
-from abc import ABC, abstractmethod, abstractproperty
-from PyQt5.QtWidgets import QApplication, QDialog
-from requests.exceptions import ConnectionError
-from tldextract import extract as extract_url
-from PyQt5.QtCore import QPropertyAnimation
-from requests.exceptions import SSLError
-from PyQt5.QtCore import QEventLoop
-from PyQt5.QtCore import QByteArray
-from PyQt5.QtCore import pyqtSignal
-from os import system as cmd_input
-from dataclasses import dataclass
-from ipaddress import ip_address
-from PyQt5.QtCore import QThread
-from PyQt5.QtCore import QObject
-from typing import Literal, Dict
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtTest import QTest
-from platform import platform
-from PyQt5 import QtWidgets
-from json import loads
-import requests
-import socket
-import sys
-import os
+except:
+    raise ModuleNotFoundError("Please activate the project main virtual environment in order to access the source code launch.")
 
 
 module_is_runned_directly = __name__ == "__main__"
