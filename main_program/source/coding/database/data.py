99434ff0449295f640c22e151b8da611e0315dfc
KhodeNima <khodenimacollab@gmail.com>
2024-01-10 15:46:10 +0330

    V : 12.0.0
    NyvoNetHunter Project | V : 12.0.0

--- main_program/source/coding/database/data.py
+++ main_program/source/coding/database/data.py
@@ -24,7 +24,7 @@ from packages import dataclass
 
 @dataclass
 class BuildData:
-    __version__ = "11.4.4"
+    __version__ = "12.0.0"
     __author__ = "KhodeNima ( Nima Bavar )"
     __build_date__ = "2023/11/13"
 
