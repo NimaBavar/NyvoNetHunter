d4f4f9b5586dac1b8f03a5817bd6b5c6695aeb80
NimaBavar <khodenimacollab@gmail.com>
2024-02-11 16:33:13 +0330

    fix(test): add test unit
    ...
    
    test: 9

--- main_program/source/coding/testing/unit_tests/test_url_object.py
+++ main_program/source/coding/testing/unit_tests/test_url_object.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """
 # The module for testing the ( `NyvoNetHunterUrl` ) class.
+
+Note that this test case does not test the url validators of the class, as their existence
+in this class is the result of composition, they are tested in test cases of their own.
 """
 
 
@@ -25,7 +28,16 @@ from database.workers.api import NyvoNetHunterUrl
 class TestNyvoNetHunterUrl(unittest.TestCase):
     def setUp(self) -> None:
         self.valid_url = NyvoNetHunterUrl("https://github.com")
+        self.url_with_path = NyvoNetHunterUrl("https://hello.com/how/are/you.com")
 
     def test_invalid_url_type(self) -> TestResult:
         with self.assertRaises(ValueError):
-            url = NyvoNetHunterUrl(34)
+            NyvoNetHunterUrl(3)
+
+    def test_path_remover(self) -> None:
+        path_removed_url = self.url_with_path.remove_paths()
+        self.assertEqual(path_removed_url, "https://hello.com")
+
+
+if __name__ == "__main__":
+    unittest.main()
