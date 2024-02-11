db4fd595451361e724c4522f9fec92c3f1f60cdb
NimaBavar <khodenimacollab@gmail.com>
2024-02-11 21:46:54 +0330

    feat(docs): add port_scan data retrieve test feat(docs): add port_scanner data retrieve test
    ...
    
    test: 19

--- main_program/source/coding/testing/unit_tests/test_port_scanner.py
+++ main_program/source/coding/testing/unit_tests/test_port_scanner.py
@@ -21,12 +21,25 @@ import unittest
 from unittest.result import TestResult
 from database.port_scanner_api.port_scanner import NyvoNetHunterPortScanner
 from database.workers.api import NyvoNetHunterUrl, NyvoNetHunterIpAddress
+from database.exceptions.port_scanner_exceptions.no_scan_history import NoScanHistoryError
 
 
 class TestPortScanner(unittest.TestCase):
+    def setUp(self) -> None:
+        self.valid_url_connectable = NyvoNetHunterUrl("https://github.com")
+        self.valid_port_scanner = NyvoNetHunterPortScanner(self.valid_url_connectable)
+            
     def test_invalid_connectable_type(self) -> TestResult:
         with self.assertRaises(ValueError):
             NyvoNetHunterPortScanner(connectable=3)
+
+    def test_data_retrieve_before_scan(self) -> TestResult:
+        with self.assertRaises(NoScanHistoryError):
+            self.valid_port_scanner.get_scan_data(data="open_ports")
+
+    def test_invalid_data_retrieve(self) -> TestResult:
+        with self.assertRaises(TypeError):
+            self.valid_port_scanner.get_scan_data(data="rain_diagram")
 
 
 if __name__ == "__main__":
