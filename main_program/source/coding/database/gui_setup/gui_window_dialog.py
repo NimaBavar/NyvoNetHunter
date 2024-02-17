41dc7cdd134c83907579a67f6faed3be1a555bf2
NimaBavar <khodenimacollab@gmail.com>
2024-02-17 18:01:53 +0330

    fix(ui): remove bugs
    ...
    
    ui_design: 13

--- main_program/source/coding/database/gui_setup/gui_window_dialog.py
+++ main_program/source/coding/database/gui_setup/gui_window_dialog.py
@@ -42,12 +42,9 @@ class Ui_Dialog(object):
         Dialog.setCursor(QtGui.QCursor(QtCore.Qt.IBeamCursor))
         Dialog.setMouseTracking(False)
         icon = QtGui.QIcon()
-<<<<<<< HEAD
         icon.addPixmap(QtGui.QPixmap("main_program/source/ui_design/resources/pictures/icon.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.On)
-=======
         icon.addPixmap(QtGui.QPixmap("main_program/source/ui_design/resources/pictures/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         icon.addPixmap(QtGui.QPixmap("main_program/source/ui_design/resources/pictures/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
->>>>>>> 7f8e7038a24b2f7fe50d7eef8848c9b7d0fdba9b
         Dialog.setWindowIcon(icon)
         Dialog.setWindowOpacity(5.0)
         self.searchButton = QtWidgets.QPushButton(Dialog)
